4ee3a012957ce52db5a13bf05f7a73f9491c642e
Sathmi1234 <devsadini@gmail.com>
2025-03-28 22:41:12 +0530

    Created student objects

---
+++ 2025-03-28/controller/StudentController.java
@@ -0,0 +1,21 @@
+package lk.ac.vau.fas.ict.controller;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lk.ac.vau.fas.ict.model.Student;
+
+@RestController
+@RequestMapping("/app")
+public class StudentController {
+	Student s1 = new Student("Bob",23,"IT","2020ict150",3.3);
+	Student s2 = new Student("Alice",22,"IT","2020ict200",3.9);
+	Student s3 = new Student("John",25,"IT","2020ict250",2.5);
+
+}
