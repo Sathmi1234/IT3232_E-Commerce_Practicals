138a133186a5f91e6620a1fda3c74be90e226a0d
Sathmi1234 <devsadini@gmail.com>
2025-03-28 22:44:04 +0530

    Created method to display all the students

--- 2025-03-28/controller/StudentController.java
+++ 2025-03-28/controller/StudentController.java
@@ -18,10 +18,20 @@ public class StudentController {
 	Student s2 = new Student("Alice",22,"IT","2020ict200",3.9);
 	Student s3 = new Student("John",25,"IT","2020ict250",2.5);
 	
+	List<Student> students = new ArrayList<Student>();
 	
 	@GetMapping("/studetails")
 	public Student getStudentDetails() {
 		return s1;
 	}
+	
+	@GetMapping("/allstudetails")
+	public List<Student> getAllStudentDetails() {
+		students.add(s1);
+		students.add(s2);
+		students.add(s3);
+		return students;
+	}
+
 
 }
