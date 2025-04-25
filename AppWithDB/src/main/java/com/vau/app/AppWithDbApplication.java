61a54a1d467f4e21e73b8eac0c9a07d14b3781e4
Sathmi1234 <devsadini@gmail.com>
2025-04-25 13:52:57 +0530

    Initialized new project with DB

---
+++ AppWithDB/src/main/java/com/vau/app/AppWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.vau.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWithDbApplication.class, args);
+	}
+
+}
