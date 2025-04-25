bacabfbeb1d53d44d9c7eb9a530c5bfa7cf764a1
Sathmi1234 <devsadini@gmail.com>
2025-04-25 14:19:49 +0530

    Creating employee model

---
+++ AppWithDB/src/main/java/com/vau/app/day6/model/Employee.java
@@ -0,0 +1,19 @@
+package com.vau.app.day6.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Employee {
+	@Id
+	private String empId;
+	
+	private String name;
+	private int age;
+	private double salary;
+}
