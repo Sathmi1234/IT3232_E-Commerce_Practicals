6c319e9b37f32eb862aeb05eca0767d68a6bfc00
Sathmi1234 <devsadini@gmail.com>
2025-04-25 15:09:07 +0530

    Modified the manytomany table creating of project, employee relationship using JoinTable and JoinColumn

--- AppWithDB/src/main/java/com/vau/app/model/Project.java
+++ AppWithDB/src/main/java/com/vau/app/model/Project.java
@@ -5,6 +5,8 @@ import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 
 @Entity
@@ -15,6 +17,11 @@ public class Project {
 	@Column(name="Project_Name")
 	private String name;
 	
+	private long totalCost;
+	
 	@ManyToMany
+	@JoinTable(name="project_assignments",
+	joinColumns = @JoinColumn(name="empid"),
+	inverseJoinColumns = @JoinColumn(name="pro_id"))
 	private List<Employee> employees;
 }
