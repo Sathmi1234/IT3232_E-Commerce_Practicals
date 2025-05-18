eddcb3f017e3e2390c03aeb50abbcce35267a62b
Sathmi1234 <devsadini@gmail.com>
2025-05-18 13:57:48 +0530

    Created many to many relationship between canteen and employee

--- AppWithDB/src/main/java/com/vau/app/model/Canteen.java
+++ AppWithDB/src/main/java/com/vau/app/model/Canteen.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Canteen {
@@ -12,7 +14,9 @@ public class Canteen {
 
     private String location;
 
+    @ManyToMany(mappedBy = "canteens")
     private List<Employee> employees;
 
+    @OneToMany(mappedBy = "canteen")
     private List<DailyMenu> dailyMenus;
 }
--- AppWithDB/src/main/java/com/vau/app/model/Employee.java
+++ AppWithDB/src/main/java/com/vau/app/model/Employee.java
@@ -3,10 +3,19 @@ package com.vau.app.model;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Employee extends Person {
     private String jobPosition;
  
+    @ManyToMany
+    @JoinTable(
+        name = "employee_canteen",
+        joinColumns = @JoinColumn(name = "employee_id"),
+        inverseJoinColumns = @JoinColumn(name = "canteen_id")
+    )
     private List<Canteen> canteens;
 }
