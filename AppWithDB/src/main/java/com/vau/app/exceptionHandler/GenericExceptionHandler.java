28f83c9c3b3707d0caa2cc3d929f42a063d96e5f
Sathmi1234 <devsadini@gmail.com>
2025-05-28 14:31:51 +0530

    Add GenericExceptionHandler for centralized exception handling (EntityNotFoundException, DuplicateKeyException and General Exception)

---
+++ AppWithDB/src/main/java/com/vau/app/exceptionHandler/GenericExceptionHandler.java
@@ -0,0 +1,33 @@
+package lk.ac.vau.fas.ict.exceptionHandler;
+
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+import lk.ac.vau.fas.ict.model.ErrorResponse;
+
+@RestControllerAdvice
+public class GenericExceptionHandler {
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<ErrorResponse>handleEntityNotFound(EntityNotFoundException exception){
+		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(),exception.getMessage());
+		return new ResponseEntity<ErrorResponse>(errorResponse,HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(DuplicateKeyException.class)
+	public ResponseEntity<ErrorResponse>handleDuplicateKey(DuplicateKeyException exception){
+		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(),exception.getMessage());
+		return new ResponseEntity<ErrorResponse>(errorResponse,HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse>handleCommonException(Exception exception){
+		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),exception.getMessage());
+		return new ResponseEntity<ErrorResponse>(errorResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
